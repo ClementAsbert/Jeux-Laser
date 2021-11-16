5ce91f08406a0955b18aa4a2716f818ef3bdb364
Moi moi <pro.yanis01@gmail.com>
2021-11-16 21:33:23 +0100

    Modification du canon

--- LaserGame/Canon.cpp
+++ LaserGame/Canon.cpp
@@ -1,4 +1,24 @@
 #include "Canon.h"
 
-Canon::Canon(int x,int y)
+Canon::Canon(int x,int y) : d_x{x}, d_y{y}, d_c{'#'}
 {}
+int Canon::getX() const
+{
+    return d_x;
+}
+
+int Canon::getY() const
+{
+    return d_y;
+}
+
+char Canon::getChar() const
+{
+    return d_c;
+}
+
+char Canon::setChar(char c) const
+{
+    d_c = c;
+}
+
--- LaserGame/Canon.h
+++ LaserGame/Canon.h
@@ -5,6 +5,13 @@
 class Canon : public Case{
 public:
     Canon(int x,int y);
+    virtual int getX() const override;
+    virtual int getY() const override;
+    virtual char getChar() const override;
+    virtual char setChar(char c) const override;
+private:
+    int d_x, d_y;
+    char d_c;
 };
 
 #endif // Canon_H_INCLUDED
